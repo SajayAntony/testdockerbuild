ebee6b3061fa890ffc5811b271bc810b3017a2a7
Sajay Antony <sajaya@microsoft.com>
2017-04-27 17:07:57 -0700

    Added sigint

--- sample/app.js
+++ sample/app.js
@@ -12,3 +12,7 @@ server.listen(8080);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8080/");
+
+process.on('SIGINT', function() {
+    process.exit();
+});
